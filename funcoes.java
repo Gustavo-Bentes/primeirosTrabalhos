513fae6727882350a265ff23eb1bc5b20be0e152
Gustavo-Bentes <gustavobentes05@gmail.com>
2022-12-16 17:51:26 -0300

    fistr commit

---
+++ funcoes.java
@@ -0,0 +1,32 @@
+public class funcoes {
+    public static void main(String[] args){
+    
+    sum(10, 15);  
+    sum(26, 189);
+    sum(3213, 421443);
+    sum(139848342, 234209985);
+
+
+    }
+     static void sum(int x, int y) { // (argumentos)
+        
+        System.out.println(x + y); 
+        System.out.println(x - y);
+        System.out.println(x / y); 
+        System.out.println(x * y);
+        }
+
+
+
+        
+
+
+
+
+     }
+
+// sum -> nome da função, geralmente usar verbos para definir função. 
+// () > usamos para definir o que chegará naquela função. (parametros)
+// tipo nomeDaVariavel 
+// {}> bloco de codigo que será executado 
+
